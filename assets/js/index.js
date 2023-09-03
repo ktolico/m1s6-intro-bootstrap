e88b61afa5e9d69634a5ecfad07e1662c23f639f
ktolico <130526956+ktolico@users.noreply.github.com>
2023-09-03 10:09:07 -0500

    class

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,18 @@
-console.log("Entro index.js");
+const arreglo1 = ["Tamales", "empanadas", "carimañolas", "Genova", "Buñuelo"];
+
+console.log(arreglo1[0], "Posicion 0");
+console.log(arreglo1[1], "Posicion 1");
+console.log(arreglo1[2], "Posicion 2");
+console.log(arreglo1[3], "Posicion 3");
+console.log(arreglo1[4], "Posicion 4");
+
+
+console.log(`La comida del dia es: ${arreglo1[3]}`)
+
+
+// mutar (cambiar) de valor un elem ento del arreglo
+
+arreglo1[4] = "chunchullo";
+console.log(arreglo1[4], "Nueva posicion 4");
+console.log("Mutacion de arreglo", arreglo1);
+
---
+++ assets/js/interacciones.js
@@ -0,0 +1,21 @@
+console.log("entro a archivo iteraciones.js");
+
+
+let nombres1 = [
+    "Norberto",
+    "Katherine",
+    "Karla",
+    "Daniel",
+    "Alfredo",
+    "Jose",
+    "Daniela",
+    "Berenice",
+    "Adrian"
+];
+
+console.log("Elementos arreglo:", nombres1.length);
+
+for (let index = 0; index < nombres1.length; index++) {
+    console.log(nombres1[index]);
+    
+}
